6bc38841d813f684b769ebc1c3309aa44f244ba7
Asim Zaidi <asimzaidih@gmail.com>
2019-03-06 23:32:27 -0800

    removed logs and old code commented out

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,17 +1,6 @@
 // src/js/reducers/index.js
 
-// import { combineReducers } from "redux";
-// import authReducer from "./authReducer";
-// import errorReducer from "./errorReducer";
-
-
-// export default combineReducers({
-//   auth: authReducer,
-//   errors: errorReducer
-// });
-
 import { HANDLE_LOGIN, SIGNUP_USER, LOGOUT_USER, HANDLE_SETTINGS, REQUEST_CLUB, VIEW_ALL_CLUBS, GET_LEADER_CLUB } from "../constants/action-types";
-
 
 const initialState = {
   token: "",
@@ -23,24 +12,18 @@ const initialState = {
 function rootReducer(state = initialState, action) {
   switch(action.type) {
     case HANDLE_LOGIN:
-    console.log("IN HANDLE_LOGIN CASE");
-      return {...state, token: action.token_payload, user: action.user_payload}
+      return {...state, user: action.payload}
     case SIGNUP_USER:
       return {...state, token: action.token_payload, user: action.user_payload}
     case LOGOUT_USER:
       return {...state, user: "", token: "", clubs: []}
     case HANDLE_SETTINGS:
-      console.log("IN HANDLE_SETTINGS CASE");
-    
       return {...state, user: action.payload}
     case REQUEST_CLUB:
-      console.log("IN VIEW_ALREQUEST_CLUBL_CLUBS CASE");
       return {...state, user: action.payload}
     case VIEW_ALL_CLUBS:
-      console.log("IN VIEW_ALL_CLUBS CASE");
       return {...state, clubs: action.payload}
     case GET_LEADER_CLUB:
-      console.log("IN GET_LEADER_CLUB CASE");
       return {...state, leaderClub: action.payload}
     default: 
         return state;
