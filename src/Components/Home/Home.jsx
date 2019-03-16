18177ed544aee6a6bae634c62cddb7999702f0f1
Asim Zaidi <asimzaidih@gmail.com>
2019-03-15 22:56:38 -0500

    Added redirect based on user at Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import '../../Styles/home.scss';
 import logo from '../../Images/logo-only.png';
-
+import { Redirect } from 'react-router';
+import { connect } from 'react-redux';
 class Home extends Component {
     render() {
-        return (          
-        <div className="home-page">
-            <img src={logo} alt="Make School"></img>
-            <h1 className="h1-primary">Clubs App</h1>
-            <button className="black_btn" onClick={() => window.location.href = '/login'}>Login</button>
-            <button className="black_btn" onClick={() => window.location.href = '/signup'}>Signup</button>
-        </div>
-        );
+        if (this.props.user) {
+            return <Redirect to='/dashboard' />
+        } else {
+            return (          
+                <div className="home-page">
+                    <img src={logo} alt="Make School"></img>
+                    <h1 className="h1-primary">Clubs App</h1>
+                    <button className="black_btn" onClick={() => window.location.href = '/login'}>Login</button>
+                    <button className="black_btn" onClick={() => window.location.href = '/signup'}>Signup</button>
+                </div>
+            );
+        };
     };
 };
 
-export default Home;
+const mapStateToProps = state => { return { user: state.user }; };
+export default connect(mapStateToProps, null)(Home);
