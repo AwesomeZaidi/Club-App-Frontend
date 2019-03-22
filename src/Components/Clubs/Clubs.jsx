dcbc619975784761a324e57845fc34f1c40c537b
Asim Zaidi <asimzaidih@gmail.com>
2019-03-21 17:04:23 -0700

    clubs list page designed

--- src/Components/Clubs/Clubs.jsx
+++ src/Components/Clubs/Clubs.jsx
@@ -7,11 +7,18 @@ import logo from '../../Images/logo-only.png';
 class Clubs extends Component {
     render() {
         return (
-            <section class='top-down-center-section'>
-                <div class='logo-title-align'>
-                    <img className="med-logo-only" src={logo} alt="Make School"></img>
-                    <h1>All Clubs</h1>
+            <section className='push-down top-down-center'>
+                <div className='logo-title-align'>
+                    <img className="small-logo-only" src={logo} alt="Make School"></img>
+                    <h1 className='h1-primary'>All Clubs</h1>
                 </div>
+                <ul className='top-down-center'>
+                    <span className='push-down list-item'>Test Club</span>
+                    <span className='push-down list-item'>Test Club</span>
+                    <span className='push-down list-item'>Test Club</span>
+                    <span className='push-down list-item'>Test Club</span>
+                    <span className='push-down list-item'>Test Club</span>
+                </ul>
             </section>
         );
     };
