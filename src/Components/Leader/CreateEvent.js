e401698c3a1eccbd87abe2936930b78bb94cc035
Asim Zaidi <asimzaidih@gmail.com>
2019-03-23 16:45:36 -0700

    create event form inputs

--- src/Components/Leader/CreateEvent.js
+++ src/Components/Leader/CreateEvent.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Redirect } from 'react-router';
-import axios from 'axios';
 import { createEvent } from "../../js/actions/index";
 import logo from '../../Images/logo-only.png';
 import '../../Styles/dashboard.scss';
+import axios from 'axios';
 
 class CreateEvent extends Component {
     constructor(props) {
@@ -12,6 +12,11 @@ class CreateEvent extends Component {
 
         this.state = {
             title: '',
+            description: '',
+            date: '',
+            timeStart: '',
+            timeEnd: '',
+            location: '',
             eventCreated: false
         };
     };
@@ -26,8 +31,8 @@ class CreateEvent extends Component {
 
     handleSubmit = async (e) => {
         e.preventDefault();
-        const eventData = this.state.title;
-        await axios.post(`/event`, eventData).then((res) => {
+        const eventData = this.state;
+        await axios.post(`/event`, eventData).then((res) => {   
             this.setState({
                 eventCreated: res.data.event._id
             });
@@ -43,7 +48,12 @@ class CreateEvent extends Component {
                 <img className="med-logo-only" src={logo} alt="Make School"></img>
                 <h1>Add Event</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <input type='text' name='title' id='title' placeholder='Title' value={this.state.title} onChange={this.handleChange} />                
+                    <input type='text' name='title' id='title' placeholder='Title' value={this.state.title} onChange={this.handleChange} />     
+                    <input type='text' name='description' id='description' placeholder='Description' value={this.state.description} onChange={this.handleChange} />       
+                    <input type='date' name='date' id='date' value={this.state.date} onChange={this.handleChange} />                
+                    <input type='time' name='timeStart' id='timeStart' value={this.state.timeStart} onChange={this.handleChange} />                
+                    <input type='time' name='timeEnd' id='timeEnd' value={this.state.timeEnd} onChange={this.handleChange} />
+                    <input type='text' name='location' placeholder='Location' id='location' value={this.state.location} onChange={this.handleChange} />
                     <button className="black_btn" type='submit' disabled={!this.validateForm()} >Submit</button>          
                 </form>
             </div>
